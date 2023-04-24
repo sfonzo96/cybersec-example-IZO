4b005a23a67c424c8f4366d22424bdc302f5a91c
santif <santiagofonzo@live.com>
2023-04-24 20:40:52 -0300

    UPD Change names

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const downloadLink = document.createElement("a");
-  downloadLink.href = "https://cybersec-example.netlify.app/download/NotAVirus.pdf";
-  downloadLink.download = "NotAVirus.pdf";
+  downloadLink.href = "https://cybersec-example.netlify.app/download/claseCybersec.pdf";
+  downloadLink.download = "";
   document.body.appendChild(downloadLink);
   downloadLink.click();
   document.body.removeChild(downloadLink);
