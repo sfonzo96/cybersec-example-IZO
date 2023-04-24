892fbf84fa087c93f5434553270a9b0ed0e6e780
santif <santiagofonzo@live.com>
2023-04-24 20:14:19 -0300

    FIX link

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   const downloadLink = document.createElement("a");
-  downloadLink.href = "https://cybersec-example.netlify.app/NotAVirus.pdf";
+  downloadLink.href = "https://cybersec-example.netlify.app/download/NotAVirus.pdf";
   downloadLink.download = "NotAVirus.pdf";
   document.body.appendChild(downloadLink);
   downloadLink.click();
